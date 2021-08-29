322cae60b36d12df01eb6fbd497b71b66b55b9b1
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-29 14:00:41 -0400

    Fix typo

--- src/features/catalogue/catalogueAPI.js
+++ src/features/catalogue/catalogueAPI.js
@@ -1,5 +1,5 @@
-export function fetchCount() {
-  return collection = fetch('https://api.spoonacular.com/recipes/complexSearch?query=pasta&number=20&apiKey=c887fd95dcf34923b31915b722fe76c1')
+export function fetchCatalogue() {
+  return catalogue = fetch('https://api.spoonacular.com/recipes/complexSearch?query=pasta&number=20&apiKey=c887fd95dcf34923b31915b722fe76c1')
       .then(res => res.json())
       .then(posts => posts.results)
       .catch(error => { throw error; });
