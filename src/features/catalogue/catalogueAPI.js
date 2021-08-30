c90f22451b44ef80207fc637efb3f68b1b8ca94b
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-29 23:22:40 -0400

    Modifying file

--- src/features/catalogue/catalogueAPI.js
+++ src/features/catalogue/catalogueAPI.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/prefer-default-export */
 export function fetchCatalogue() {
-  return catalogue = fetch('https://api.spoonacular.com/recipes/complexSearch?query=pasta&number=20&apiKey=c887fd95dcf34923b31915b722fe76c1')
-      .then(res => res.json())
-      .then(posts => posts.results)
-      .catch(error => { throw error; });
+  const catalogue = fetch('https://api.spoonacular.com/recipes/complexSearch?query=pasta&number=20&apiKey=c887fd95dcf34923b31915b722fe76c1')
+    .then(res => res.json())
+    .then(posts => posts.results)
+    .catch(error => { throw error; });
+  return catalogue;
 }
