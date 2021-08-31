c5e70cd905c317d6ca45abe2669151b1a42089d1
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-31 19:55:59 -0400

    Fix linter

--- src/features/catalogue/catalogueAPI.js
+++ src/features/catalogue/catalogueAPI.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 export function fetchCatalogue() {
   const catalogue = fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
     .then(res => res.json())
